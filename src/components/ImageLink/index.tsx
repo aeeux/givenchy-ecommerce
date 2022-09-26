5287cf5caa972df282636100d5ede4cef1caebea
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-09-26 12:59:41 +0200

    Create index.tsx

---
+++ src/components/ImageLink/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { DataType } from '../../containers/Home'
+import './style.scss'
+import { motion } from 'framer-motion'
+import {defaultTransition} from '../../utils/transition'
+import { navigate } from 'gatsby'
+
+type Props = {
+    element: DataType,
+    index: number;
+}
+
+export default function ImageLink({index, element}: Props) {
+
+  const navigateTo = () => {
+    navigate(element.slug)
+  }
+
+
+  return (
+    <motion.img 
+    onClick={navigateTo}
+    className='image-link-item'
+    layoutId={`container-${index}`}
+    transition={defaultTransition}
+    src={element.cover}
+    />
+  )
+}
