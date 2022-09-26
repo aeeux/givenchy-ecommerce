2df52544278e3c5d69f72f93c8d4a997ff1e892c
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-09-26 17:59:19 +0200

    Update Home.tsx

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,7 +1,10 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Header from "../components/Header";
 import ImageLink from "../components/ImageLink";
+import Loader from "../components/Loader";
 import jsonData from "../data.json";
+import { motion, useAnimation, useMotionValue } from 'framer-motion'
+import { defaultTransition } from '../utils/transition'
 
 export type DataType = {
   cover: string;
@@ -10,23 +13,71 @@ export type DataType = {
   slug: string;
 };
 
+const gridUtils = [600, 400, 600, 800, 600]
+
 export default function Home() {
-  const [gridVisible, setGridVisible] = useState(false);
+  const [gridVisible, setGridVisible] = useState(true);
+  
+  const loaderControls = useAnimation()
+  const animation = useAnimation()
   const mapData: DataType[] = Array.from(jsonData);
+
+  const bgColor = useMotionValue("black")
+  useEffect(() => {
+
+    async function sequence() {
+      await animation.set((index) => ({
+        y: gridUtils[index % 5],
+        scale: 1.1,
+      }))
+
+      await animation.start((index) => ({
+        y: 0,
+        transition: defaultTransition,
+      }))
+      bgColor.set("white")
+
+
+      await animation.start({
+        scale: 1,
+        transition: defaultTransition,
+      })
+    
+      setGridVisible(false);
+
+    }
+    setTimeout(() => {
+      loaderControls.start({
+          opacity: 0,
+          transition: {defaultTransition}
+        });
+
+
+        sequence();
+    }, 2000)
+  }, [])
 
   return (
     <>
+    <Loader title={"Cities"} loaderControls={loaderControls} />
     <Header view={gridVisible} toggleView={(value) => setGridVisible(value)} />
-      <div>
+      <motion.div className="content"
+      style={{ backgroundColor: bgColor,
+      transition: 'background-color 1.25s ease-in-out'}}
+      >
         {gridVisible && (
           <div className="grid-container">
             <div className="grid-elements">
               {mapData.map((element, index) => (
-                <div className="element">
+                <motion.div className="element"
+                key={element.slug}
+                animate={animation}
+                custom={index}
+                >
                   <div className="thumbnail-wrapper">
                     <ImageLink element={element} index={index} />
                   </div>
-                </div>
+                </motion.div>
               ))}
             </div>
           </div>
@@ -42,7 +93,7 @@ export default function Home() {
             ))}
           </div>
         )}
-      </div>
+      </motion.div>
     </>
   );
 }
